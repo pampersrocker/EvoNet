bfeb37579c53b2e9b00f96993492afc42e5fed53
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2016-10-17 09:34:36 +0200

    Added Mathf for floating point math

---
+++ EvoNet/Mathf.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EvoNet
+{
+  public class Mathf
+  {
+    public static float Sin(float Value)
+    {
+      return (float)Math.Sin(Value);
+    }
+
+    public static float Cos(float Value)
+    {
+      return (float)Math.Cos(Value);
+    }
+
+    public static float Tan(float Value)
+    {
+      return (float)Math.Tan(Value);
+    }
+
+    public static float Asin(float Value)
+    {
+      return (float)Math.Asin(Value);
+    }
+
+    public static float Acos(float Value)
+    {
+      return (float)Math.Acos(Value);
+    }
+
+    public static float Atan(float Value)
+    {
+      return (float)Math.Atan(Value);
+    }
+  }
+}
